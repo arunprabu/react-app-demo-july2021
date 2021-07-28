b387cd1ddc25f5e2111f6ec7efa1321b9e678eb4
Arun Vijayarengan <arunprabu.v@hotmail.com>
2021-07-28 10:14:45 +0530

    updated

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -3,47 +3,61 @@ import axios from 'axios'
 
 class Users extends Component {
 
-    // What's the REST API URL? http://localhost:3000/api/users
-    // What is the Http Method? GET
-    // What is the REST API Client? npm i axios 
+  // What's the REST API URL? http://localhost:3000/api/users
+  // What is the Http Method? GET
+  // What is the REST API Client? npm i axios 
 
-    componentDidMount(){ // lifecycle hooks
-        console.log('Inside Component Did Mount');
-        // ideal place for you to send ajax req
-        axios.get('http://localhost:3000/api/users')
-            .then( (res) => {
-                console.log(res);
-            })
-            .catch( (error) => {
-                console.log(error)
-            })
-            .finally( () => {
-                console.log('It is over!');
-            });
-    }
+  state = {
+    users: []
+  }
 
-    render(){
-        console.log('Inside Render');
-        return(
-            <div className="container">
-                <h2>Lising Users</h2>
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                
-                            </div>
-                        </div>
+  componentDidMount() { // lifecycle hook
+    console.log('Inside Component Did Mount');
+    // ideal place for you to send ajax req
+    axios.get('http://localhost:3000/api/users')
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          users: res.data
+        });
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      .finally(() => {
+        console.log('It is over!');
+      });
+  }
+
+  render() {
+    console.log('Inside Render');
+    return (
+      <div className="container">
+        <h2>Lising Users</h2>
+        <div className="row">
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body">
+                {
+                  this.state.users && this.state.users.length > 0 ?
+                    <div>
+                      <h5 className="card-title">{this.state.users[0].fullName}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">{this.state.users[0].email}</h6>
+                      <p className="card-text">{this.state.users[0].phone}</p>
                     </div>
+                    :
+                    <div>Unable to load users</div>
+                }
 
-                    
-                </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+
+
+        </div>
+      </div>
+    )
+  }
 
 }
 
